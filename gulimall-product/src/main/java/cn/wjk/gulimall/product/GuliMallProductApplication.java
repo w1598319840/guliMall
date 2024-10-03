43a43fc67ed391b8c38e8848935443954ff9c5c1
w159 <1598319840@qq.com>
2024-10-03 14:12:08 +0800

    init

---
+++ gulimall-product/src/main/java/cn/wjk/gulimall/product/GuliMallProductApplication.java
@@ -0,0 +1,13 @@
+package cn.wjk.gulimall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuliMallProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuliMallProductApplication.class, args);
+    }
+
+}
